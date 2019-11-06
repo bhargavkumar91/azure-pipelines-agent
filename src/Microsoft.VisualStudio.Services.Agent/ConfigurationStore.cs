bd56429c2319e3ba1a4dbb9d4dd72108c4233678
Stephen Franceschelli <stephenmichaelf@gmail.com>
2019-11-06 14:14:17 -0500

    Note.

--- src/Microsoft.VisualStudio.Services.Agent/ConfigurationStore.cs
+++ src/Microsoft.VisualStudio.Services.Agent/ConfigurationStore.cs
@@ -25,7 +25,7 @@ namespace Microsoft.VisualStudio.Services.Agent
         public bool IsHosted => !string.IsNullOrEmpty(NotificationPipeName) || !string.IsNullOrEmpty(NotificationSocketAddress);
 
         [DataMember(EmitDefaultValue = false)]
-        public string Fingerprints { get; set; }
+        public string Fingerprints { get; set; } // TODO: Make this array of strings?
 
         [DataMember(EmitDefaultValue = false)]
         public string NotificationPipeName { get; set; }
